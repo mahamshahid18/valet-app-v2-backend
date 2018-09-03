f3bbb2c9a855888f91b8d6b5c8fb3af7a46e3e92
Maham Shahid <mahum.shahid934@gmail.com>
2018-09-03 22:29:16 +0500

    Updated db connection string

--- server/db/DbConnector.js
+++ server/db/DbConnector.js
@@ -7,11 +7,17 @@ class DbConnector {
         this.serverName = process.env.DB_BASE_URL;
         this.port = process.env.DB_PORT;
         this.dbName = process.env.DB_NAME;
+        this.dbUser = process.env.DB_USER;
+        this.dbPwd = process.env.DB_PWD;
     }
 
     connect() {
         const dbUri = `${this.serverName}:${this.port}`;
-        return mongoose.connect(`mongodb://${dbUri}/${this.dbName}`, { useNewUrlParser: true });
+
+        return mongoose.connect(
+            `mongodb://${this.dbUser}:${this.dbPwd}@ds022408.${dbUri}/${this.dbName}`,
+            { useNewUrlParser: true }
+        );
     }
 }
 
